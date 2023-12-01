6050be4570a657a00ea5b7c584891089332b1518
unknown <mark.cann@ymail.com>
2023-12-01 14:48:25 +0000

    choco install googlechrome --version 119.0.6045.160 -y choco install python3 --version 3.12.0 -y choco install 7zip.install --version 23.1.0 -y choco install vscode --version 1.84.2 -y

---
+++ choco_packages_install.ps1
@@ -0,0 +1,9 @@
+# not exactly powershell, but enough to install
+
+choco install googlechrome -y
+choco install python3 -y
+choco install 7zip.install -y
+choco install vscode -y
+choco install packer -y
+choco install terraform -y
+
